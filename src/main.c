20f261268652555e730c1915362e0a6a541c797b
Luka <luka68231@gmail.com>
2026-06-06 12:23:45 -0400

    Update main.c

--- src/main.c
+++ src/main.c
@@ -6,3 +6,11 @@
 
 #define DELAY_ms 3
 
+static inline void initTimer0(void)
+{
+    TCCR0B |= (1 << CS01) | (1 << CS00);
+
+    TIMSK0 |= ((1 << OCIE0A) | (1 << OCIE1B));
+    TIMSK0 |= (1 << TOIE0);
+    sei();
+}
