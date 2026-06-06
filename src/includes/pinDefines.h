8593e499dfcd3ad3eb6d60643e348e92d051fe22
Luka <luka68231@gmail.com>
2026-06-06 14:39:12 -0400

    up

--- src/includes/pinDefines.h
+++ src/includes/pinDefines.h
@@ -1,5 +1,8 @@
 #ifndef PIN_DEFINES_H
 #define PIN_DEFINES_H
 
+#define LED_DDR DDRB
+#define LED_PORT PORTB
+
 
 #endif
--- src/main.c
+++ src/main.c
@@ -17,3 +17,43 @@ static inline void initTimer0(void)
     TIMSK0 |= (1 << TOIE0);
     sei();
 }
+
+ISR(TIMER0_OVF_vect)
+{
+    LED_PORT = 0xff;
+    OCR0A = brightnessA;
+    OCR0B = brightnessB;
+}
+ISR(TIMER0_COMPA_vect)
+{
+    LED_PORT &= 0b11110000;
+}
+ISR(TIMER0_COMPB_vect)
+{
+    LED_PORT &= 0b00001111;
+}
+
+int main(void)
+{
+    uint8_t i;
+    LED_DDR = 0xff;
+    initTimer0();
+
+    while (1)
+    {
+        for (i = 0; i < 255; i++)
+        {
+            _delay_ms(DELAY_ms);
+            brightnessA = i;
+            brightnessB = 255 - i;
+        }
+
+        for (i = 254; i > 0; i--)
+        {
+            _delay_ms(DELAY_ms);
+            brightnessA = i;
+            brightnessB = 255 - i;
+        }
+    }
+    return 0;
+}
