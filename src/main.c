557cc7bb3d225c287907de68ba6c0cc241a35873
Luka <luka68231@gmail.com>
2026-06-12 15:28:53 -0400

    quick fix

--- src/main.c
+++ src/main.c
@@ -13,7 +13,7 @@ static inline void initTimer0(void)
 {
     TCCR0B |= (1 << CS01) | (1 << CS00);
 
-    TIMSK0 |= ((1 << OCIE0A) | (1 << OCIE1B));
+    TIMSK0 |= ((1 << OCIE0A) | (1 << OCIE0B));
     TIMSK0 |= (1 << TOIE0);
     sei();
 }
